1ef9327c1fc0ab88d76b0d6e0aec07ca7f76f1c5
SKYLAR DEITZ <sd1081@students.necsd.net>
2020-12-07 15:12:42 +0000

    Problem 2

--- Prob2.java
+++ Prob2.java
@@ -3,6 +3,18 @@ import java.util.Scanner;
 public class Prob2 {
   public static void main(String[] args){
 
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter two integers:");
+
+    int int1 = scan.nextInt();
+    int int2 = scan.nextInt();
+
+    if (int1 > 0 && int2 > 0)
+      System.out.println("Both are positive or zero.");
+
+    else
+      System.out.println("One or both are negative.");
+
   }
   
   }
