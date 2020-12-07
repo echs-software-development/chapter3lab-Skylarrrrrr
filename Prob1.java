ee71014b71b9d6290a0cc8b4222a55227825df56
SKYLAR DEITZ <sd1081@students.necsd.net>
2020-12-07 15:04:59 +0000

    Problem 1

--- Prob1.java
+++ Prob1.java
@@ -3,6 +3,16 @@ import java.util.Scanner;
 public class Prob1 {
   public static void main(String[] args){
 
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter a number:");
+
+    int number = scan.nextInt();
+    
+    if (number > 5 && number < 76)
+      System.out.println("False");
+
+    else
+      System.out.println("True");
 
 
   }
