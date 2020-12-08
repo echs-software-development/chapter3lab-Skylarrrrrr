600401e55584746fbc8e021c373a755046ac8717
SKYLAR DEITZ <sd1081@students.necsd.net>
2020-12-08 04:28:52 +0000

    Problem 3

--- Prob3.java
+++ Prob3.java
@@ -2,13 +2,18 @@ import java.util.Scanner;
 
 public class Prob3 {
   public static void main(String[] args) {
-    
-    
+    //BMI <18.5 Underweight
+    //18.5< BMI <25.0 Normal
+    //25.0 < BMI <30.0 Overweight
+    //30.0 < BMI Obese
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter weight in pounds:");
     // Prompt the user to enter weight in pounds
-    
-    
-    // Prompt the user to enter height in inches
+    double weight = scan.nextDouble();
 
+    System.out.println("Enter height in inches:");
+    // Prompt the user to enter height in inches
+    double height = scan.nextDouble(); 
 
     
     //Constant
@@ -21,6 +26,17 @@ public class Prob3 {
     double BMI = weightInKilograms/(heightInMeters * heightInMeters);
 
     // Display result using if, else if and else
-   
+    if (BMI < 18.5)
+      System.out.println("Your BMI is underweight.");
+
+    else if (BMI < 25.0 && BMI > 18.5)
+      System.out.println("Your BMI is normal.");
+
+    else if (BMI < 30.0 && BMI > 25.0)
+      System.out.println("Your BMI is overweight.");
+
+    else if (BMI > 30.0)
+      System.out.println("Your BMI is obese.");
+
   }
 }
